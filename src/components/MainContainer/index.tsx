9c2a3f343e8477e33dbdce56421429e15dc89099
VitorLSBrandao <vitor.brandao@eduzz.com>
2021-09-09 21:13:54 -0300

    feat:add main container

---
+++ src/components/MainContainer/index.tsx
@@ -0,0 +1,11 @@
+
+import styles from './styles.module.scss';
+
+export default function MainContainer() {
+    return (
+        <main className={styles.container}>
+            <h1>AV Solutions</h1>
+        </main>
+    );
+}
+
---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import MainContainer from "../components/MainContainer";
+
+export default function Home() {
+  return (
+    <MainContainer />
+  );
+}
+
