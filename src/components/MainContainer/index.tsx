c46d337389faf5e9fc87be7c22f9ea72711d6138
VitorLSBrandao <vitor.brandao@eduzz.com>
2021-09-22 11:56:07 -0300

    refactor: componentization of the profile

--- src/components/MainContainer/index.tsx
+++ src/components/MainContainer/index.tsx
@@ -2,21 +2,46 @@ import styles from "./styles.module.scss";
 import About from "../About";
 import Footer from "../Footer";
 import React from "react";
-import Alex from "../Alex";
-import Vitor from "../Vitor";
 import Salve from "../Salve";
+import Profile from "../Profile";
+
+interface IUser {
+  name: string;
+  fullName: string;
+  description: string;
+  imageOrder: ImagePosition;
+}
+
+enum ImagePosition {
+  right,
+  left,
+}
 
 export default function MainContainer() {
+  const alex: IUser = {
+    name: "alexercolin",
+    fullName: "Alex Ercolin Oliveira",
+    description: "Alex Ercolin Oliveira",
+    imageOrder: ImagePosition.left,
+  };
+
+  const vitor: IUser = {
+    name: "ViBrandao",
+    fullName: "Vitor Lima Siqueira Brandão",
+    description: "Vitor Lima Siqueira Brandão",
+    imageOrder: ImagePosition.right,
+  };
+
   return (
     <div>
       <div id="about" className={styles.container}>
         <About />
       </div>
       <div id="alex" className={styles.container}>
-        <Alex />
+        <Profile user={alex} />
       </div>
       <div id="vitor" className={styles.container}>
-        <Vitor />
+        <Profile user={vitor} />
       </div>
       <div id="salve" className={styles.container}>
         <Salve />
