8475dae59eb95e963624e1d4f360bb560f74ec2f
alexercolin <alex_ercolin@hotmail.com>
2021-09-29 16:09:19 -0300

    feat: add new text to Alex Component e calculate living days for both

--- src/components/MainContainer/index.tsx
+++ src/components/MainContainer/index.tsx
@@ -17,12 +17,16 @@ enum ImagePosition {
   left,
 }
 
+
 export default function MainContainer() {
+  const livingDaysAlex = Math.round((new Date().getTime() - new Date(1997, 11, 14).getTime()) / 86400000)
+  const livingDaysVitor = Math.round((new Date().getTime() - new Date(1993, 9, 13).getTime()) / 86400000)
+
   const alex: IUser = {
     name: "alexercolin",
     fullName: "Alex Ercolin Oliveira",
     description:
-      "Primeiro eu queria cumprimentar os internautas. -Oi Internautas! Depois dizer que o meio ambiente é sem dúvida nenhuma uma ameaça ao desenvolvimento sustentável. E isso significa que é uma ameaça pro futuro do nosso planeta e dos nossos países. O desemprego beira 20%, ou seja, 1 em cada 4 portugueses.A população ela precisa da Zona Franca de Manaus, porque na Zona franca de Manaus, não é uma zona de exportação, é uma zona para o Brasil. Portanto ela tem um objetivo, ela evita o desmatamento, que é altamente lucravito. Derrubar arvores da natureza é muito lucrativo!",
+      `Olá usuários da internet ✌️ Meu nome é Alex. Sou formado em Engenharia de Produção pela FACENS. Tenho dias de idade ${livingDaysAlex} (a gente fez a continha com js). Iniciei minha trajetória profissional  ̶f̶a̶z̶e̶n̶d̶o̶ ̶m̶u̶i̶t̶o̶s̶ ̶c̶a̶f̶é̶s̶ no setor de vendas e passei um bom tempo em engenheria de processos, mas sempre com um pé em Tecnologia. Aí o gerente ficou louco e decidiu começar do zero e se tornar um desenvolvedor. Atualmente atuo como Full-Stack Developer (nome chique né?) trabalhando com a minha linguagem de programação favorita: JavaScript. `,
     imageOrder: ImagePosition.left,
   };
 
@@ -30,7 +34,7 @@ export default function MainContainer() {
     name: "ViBrandao",
     fullName: "Vitor Lima Siqueira Brandão",
     description:
-      "Primeiro eu queria cumprimentar os internautas. Oi Internautas! 👋 Me chamo Vitor. Sou formado em Análise e Desenvolvimento de Sistemas pela Fatec Sorocaba. Tenho 'minha idade aqui' (a gente fez a continha com js) dias de idade. Iniciei minha jornada profissional fazendo aquele tipo de 'serviço de estagiário', onde resolvia pequenos bugs e fui apresentado a santíssima trindade do front end:  html, css e javascript. Desde então,  ̶f̶o̶i̶ ̶s̶ó̶ ̶l̶a̶d̶e̶i̶r̶a̶ ̶a̶ ̶b̶a̶i̶x̶o̶ evoluí demais. Hoje atuo como desenvolvedor Full Stack. As linguagens que mais gosto de trabalhar são: C# e JavaScript.",
+      `Primeiro eu queria cumprimentar os internautas. Oi Internautas! 👋 Me chamo Vitor. Sou formado em Análise e Desenvolvimento de Sistemas pela Fatec Sorocaba. Tenho ${livingDaysVitor} dias de idade (a gente fez a continha com js). Iniciei minha jornada profissional fazendo aquele tipo de 'serviço de estagiário', onde resolvia pequenos bugs e fui apresentado a santíssima trindade do front end:  html, css e javascript. Desde então,  ̶f̶o̶i̶ ̶s̶ó̶ ̶l̶a̶d̶e̶i̶r̶a̶ ̶a̶ ̶b̶a̶i̶x̶o̶ evoluí demais. Hoje atuo como desenvolvedor Full Stack. As linguagens que mais gosto de trabalhar são: C# e JavaScript.`,
     imageOrder: ImagePosition.right,
   };
 
